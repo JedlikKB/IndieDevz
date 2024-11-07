dcea83f7804972b861520d9b5baf2337572e6cb3
Csokigyerek <60391120+JedlikKB@users.noreply.github.com>
2024-11-07 20:41:26 +0100

    Kamera mozgás
    A kamera folyamatos mozgása jobbra, úgy hogy az űrhajó nem tud kimenni a kamera látómezejéből. A margók változtathatóak későbbi optimalizálásnál.

---
+++ IndieDevzGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class PlayertController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f; // A mozgás sebessége (a playert kiválasztva a scriptnél manuálisan változtatható)
+
+    private void Update()
+    {
+        // Bemeneti értékek beolvasása (W, A, S, D vagy a nyilak, unityben alapból meg vannak adva)
+        float moveX = Input.GetAxis("Horizontal"); // Vízszintes mozgás (az x tengelyen)
+        float moveY = Input.GetAxis("Vertical");   // Függőleges mozgás (az y tengelyen)
+
+        // Mozgás irányának kiszámítása
+        Vector2 movement = new Vector2(moveX, moveY); //átlósan is tudunk haladni ezzel
+
+        // Mozgás végrehajtása
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+
+        
+    }
+}
